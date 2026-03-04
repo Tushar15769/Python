d5426a22bacf007afdcb60cce350d3d81cc78fa6
Your Name <tushar.18246@gmail.com>
2026-03-04 21:14:07 +0530

    Take input for the marks for 3 subjects and store them in a dictionary

---
+++ New.py
@@ -0,0 +1,12 @@
+marks = {}
+
+x = int(input("Enter phy : "))
+marks.update({"phy" : x})
+
+x = int(input("Enter chem : "))
+marks.update({"chem" : x})
+
+x = int(input("Enter maths : "))
+marks.update({"maths" : x})
+
+print(marks)
