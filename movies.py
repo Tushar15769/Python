dbcb697341bccbd0d622067ad4b1503b0dc6d9f3
Your Name <tushar.18246@gmail.com>
2026-03-04 19:04:06 +0530

    Take the input from the user for 3 movies and store them in a list

---
+++ movies.py
@@ -0,0 +1,10 @@
+movies = []
+mov1 = input("Enter the 1st movie : ") 
+mov2 = input("Enter the 2nd movie : ") 
+mov3 = input("Enter the 3rd movie : ") 
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
