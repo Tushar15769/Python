2f9cfd7ad383b493f3c1082fc9c3a850ccdd2093
Your Name <tushar.18246@gmail.com>
2026-03-30 23:08:07 +0530

    Program for searching an element in the list

--- Search.py
+++ Search.py
@@ -2,7 +2,7 @@ list_1=[1 ,4 ,9 ,16 ,25 ,36 ,49 ,64 ,81 ,100 ]
 i = 0
 x = int(input("Enter the number from the list:"))
 
-while(i<len(list_1)):
+while(i < len(list_1)):
     if(list_1[i]==x):
       print(f"{x} found at index {i}")
       break
