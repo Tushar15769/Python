e3ba86b3823f629a88d0c0de47137281379a52fa
Your Name <tushar.18246@gmail.com>
2026-07-21 19:07:20 +0530

    Finding sum of first n natural numbers using while loop

--- Voting.py
+++ Voting.py
@@ -3,4 +3,4 @@ age = int(input("Enter the number : "))
 if (age >= 18) :
   print("You are Eligible for voting")
 else :
-  print("You are not eligible for voting")
+  print("You are not eligible for voting") 
